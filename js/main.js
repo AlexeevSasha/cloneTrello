91e20287da10745cedb618ab7100e1a15d464c6a
AlexeevSasha <work.alexeevs@gmail.com>
2021-10-13 15:48:43 +0300

    add dragNDrop

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const lists = document.querySelectorAll('.list');
+const btnBoard = document.querySelector('.button')
 
 // add Task
 function addTask() {
@@ -42,8 +43,85 @@ function addTask() {
         btn.style.display = 'flex'
         textarea.value = '';
         value = '';
+        dragNDrop()
     })
 
 }
 
 addTask()
+
+//add a board
+function addBoard () {
+    const boards = document.querySelector('.boards');
+    const board = document.createElement('div');
+    board.classList.add('boards__item');
+    board.innerHTML = `
+        <span class="title" contenteditable="true">Введите название</span>
+        <div class="list"></div>
+    `
+    boards.appendChild(board)
+    clearTitle();
+    dragNDrop();
+}
+btnBoard.addEventListener('click', addBoard)
+
+//clear title board
+
+function clearTitle() {
+    const titles = document.querySelectorAll('.title')
+    titles.forEach(title => {
+        title.addEventListener('click', e => {
+            e.target.textContent = '';
+        })
+    })
+}
+clearTitle()
+
+
+//DragNDrop
+let draggedItem = null;
+function dragNDrop() {
+    const listItems = document.querySelectorAll('.list__item');
+    const lists = document.querySelectorAll('.list');
+
+    for(let i = 0; i < listItems.length; i++) {
+        const item = listItems[i];
+
+        item.addEventListener('dragstart', () => {
+            draggedItem = item
+            setTimeout(() => {
+                item.style.display = 'none'
+            }, 0)
+        })
+
+        item.addEventListener('dragend', () => {
+            setTimeout(() => {
+                item.style.display = 'block';
+                draggedItem = null;
+            }, 0)
+        })
+
+        //delete task
+        item.addEventListener('dblclick', () => {
+               item.remove();
+        })
+
+        for (let j = 0; j < lists.length; j++) {
+            const list = lists[j];
+
+            list.addEventListener('dragover', e => e.preventDefault())
+
+            list.addEventListener('dragenter', function (e) {
+                e.preventDefault();
+                this.style.backgroundColor = 'rgba(0,0,0,.1)';
+            })
+
+            list.addEventListener('dragleave', function(e) {
+                this.style.backgroundColor = 'rgba(0,0,0,0)';
+                this.append(draggedItem);
+            })
+        }
+    }
+
+}
+dragNDrop()
